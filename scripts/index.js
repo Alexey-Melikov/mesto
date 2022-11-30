11591aeb6744b6086eeff4fe5b6ec33cca161c4a
Alexey melikov <aleshkamelikov@gmail.com>
2022-11-30 10:23:53 +0300

    done

--- scripts/index.js
+++ scripts/index.js
@@ -2,19 +2,19 @@ let profileButton = document.querySelector(".profile__edit-button");
 let popup = document.querySelector(".popup");
 let closButton = popup.querySelector(".popup__close-button");
 let formEl = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__input-name");
+let nameInput = document.querySelector(".popup__input_name");
 let profileName = document.querySelector(".profile__name");
-let descriptionInput = document.querySelector(".popup__input-description");
+let descriptionInput = document.querySelector(".popup__input_description");
 let description = document.querySelector(".profile__description");
 
 function popupOpened() {
-  popup.classList.add("popup__opened");
+  popup.classList.add("popup_opened");
   nameInput.value = profileName.textContent;
   descriptionInput.value = description.textContent;
 }
 
 function popupClose() {
-  popup.classList.remove("popup__opened");
+  popup.classList.remove("popup_opened");
 }
 
 function handleFormSubmit(evt) {
@@ -29,4 +29,3 @@ profileButton.addEventListener("click", popupOpened);
 closButton.addEventListener("click", popupClose);
 
 formEl.addEventListener("submit", handleFormSubmit);
-
