52c7158c9b25ee99ae96d8ff448bc6ddcfe3fcd4
Alexey melikov <aleshkamelikov@gmail.com>
2023-02-17 15:12:41 +0300

    sprint nine develop

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin"); // подключили плагин
 
 module.exports = {
   mode: "development",
@@ -9,7 +10,7 @@ module.exports = {
   output: {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "build"),
-    clean: true, // очищаем папку билд от старого кода
+    //clean: true, // очищаем папку билд от старого кода (тперь мы подключили плагин: CleanWebpackPlugin)
   },
   module: {
     rules: [
@@ -36,6 +37,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src", "index.html"),
     }),
